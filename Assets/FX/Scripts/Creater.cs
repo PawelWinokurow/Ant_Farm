2fbfdc623a1c9cae33d0479953d4f7d8393abcfa
Fjodor Vinokurov <vinnokurov@googlemail.com>
2022-10-16 21:00:47 +0300

    подтюнил

--- Assets/FX/Scripts/Creater.cs
+++ Assets/FX/Scripts/Creater.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Creater : MonoBehaviour
 {
+    //[System.Serializable]
     public class Hexagon
     {
      public bool isSelected;
@@ -13,21 +14,29 @@ public class Creater : MonoBehaviour
 
     public int higth = 10;
     public int width = 10;
-    public GameObject hexagonPrefab;
+    public MeshRenderer hexagonPrefab;
     public Hexagon[,] hexagons;
 
     // Start is called before the first frame update
     void Start()
     {
         hexagons = new Hexagon[width, higth];
-        for (int y = 0; y < higth; y++)
+        Debug.Log(hexagons[0,0]);
+        int x=0;
+        int y = 0;
+        for (y = 0; y < higth; y++)
         {
-            for (int x = 0; x < width; x++)
+            for (x = 0; x < width; x++)
             {
-                hexagons[x,y].mr = Instantiate(hexagonPrefab, transform.position+new Vector3(x*0.5f,y*0.5f,0f), transform.rotation,transform).GetComponent<MeshRenderer>();
+                Hexagon hex= new Hexagon();
+         
+                    hex.mr = Instantiate(hexagonPrefab, transform.position + new Vector3(x * 1.5f+ 1.5f*(y%2f)/2f, y * 0.433f, 0f), transform.rotation, transform);
+
+                hexagons[x,y]= hex;
 
             }
         }
+        transform.position = new Vector3(-hexagons[x-1, y-1].mr.transform.position.x / 2f, -hexagons[x-1, y-1].mr.transform.position.y / 2f, 0f);
     }
 
     // Update is called once per frame
