5e2a1cbca8ecd6baa11a2655c480e9f21f7153bf
Pawel Winokurow <pawel.winokurow@alexanderthamm.com>
2022-11-02 13:14:06 +0100

    Spawn coroutine

--- Assets/FX/Scripts/AntsManager.cs
+++ Assets/FX/Scripts/AntsManager.cs
@@ -10,10 +10,15 @@ namespace AntFarm
         public JobScheduler JobScheduler;
         public Digger DiggerPrefab;
 
-       public  void AddAnt()
+        public void AddAnt()
         {
-            JobScheduler.AddMob(Instantiate(DiggerPrefab, new Vector3(30, 0, 30), Quaternion.identity));
+            StartCoroutine(SpawnMob());
         }
 
+        IEnumerator SpawnMob()
+        {
+            JobScheduler.AddMob(Instantiate(DiggerPrefab, new Vector3(30, 0, 30), Quaternion.identity));
+            yield return null;
+        }
     }
 }
