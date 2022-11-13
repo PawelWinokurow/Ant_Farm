d7b1830d5b5f88316a6a901434dcf73b39910b1b
Fjodor Vinokurov <vinnokurov@googlemail.com>
2022-11-13 20:40:34 +0300

    подтюнил

--- Assets/FX/Scripts/Mobs/Animators/AntAnimator.cs
+++ Assets/FX/Scripts/Mobs/Animators/AntAnimator.cs
@@ -14,31 +14,19 @@ public class AntAnimator : MonoBehaviour
     private int f;
     public MeshFilter mf;
     public MeshRenderer mr;
+    private float t;
 
     private Vector3 forward;
+    private Vector3 antBaseForwardOld;
+    private Vector3 posOld;
     private Quaternion smoothRot;
     public Transform angl;
 
-    public GameObject deadFX_prefab;
-    public HealthPopUp popUp_prefad;
-
-    MaterialPropertyBlock bodyProps;
-    MaterialPropertyBlock progressbarProps;
-    public MeshRenderer progressbar;
-
-
-    public float healthMax;
-    public float health;
-    public float healTime;
-    private float hitTime=1000f;
-    private bool isDead;
-
     private void Start()
     {
+        posOld = transform.position;
+        antBaseForwardOld = transform.forward;
         current = run;
-        bodyProps = new MaterialPropertyBlock();
-        progressbarProps = new MaterialPropertyBlock();
-        health = healthMax;
     }
 
     public void Run()
@@ -60,6 +48,8 @@ public class AntAnimator : MonoBehaviour
     {
         if (worker != null)
         {
+
+            //t += Time.deltaTime / (1f / 30f) * current.speed;
             f = (int)(Time.time * 30f * 1.5f) % current.sequence.Length;
             mf.mesh = current.sequence[f];
 
@@ -77,66 +67,5 @@ public class AntAnimator : MonoBehaviour
             mf.transform.rotation = smoothRot;
             mf.transform.Rotate(new Vector3(-90f, 0f, 180f), Space.Self);
         }
-
-        hitTime += Time.deltaTime;
-
-        if (hitTime <= 0.2f)//мигание при ударе
-        {
-
-            bodyProps.SetFloat("_Blink_FX",1f- Mathf.Clamp(hitTime/0.1f*100f/healthMax, 0f, 1f));
-            mr.SetPropertyBlock(bodyProps);
-        }
-
-        if( health < healthMax)
-        {
-            progressbar.transform.localScale = new Vector3(healthMax / 100f, 1f, 1f);
-
-            if ( hitTime > 2f)//оздоровляем если две секунды не было драки
-            {
-                health += Time.deltaTime * healthMax / healTime;
-            }
-        }
-        progressbar.enabled = !isDead && health < healthMax;//при повреждении показываем прогрессбар
-
-   
-
-        progressbarProps.SetFloat("_Health", ExtensionMethods.RemapClamp( health / healthMax,0f,1f,0.1f,1f));
-        progressbar.SetPropertyBlock(progressbarProps);
-
-    }
-
-    public void Restart()//test
-    {
-        mr.enabled = true;
-        isDead = false;
-        health = healthMax;
-
-    }
-    public void Hit(int damage)
-    {
-        hitTime = 0f;
-        health -= damage;
-        if (health > 0 )//пока живы
-        {
-
-            HealthPopUp healthPopUp = Instantiate(popUp_prefad, transform.position, transform.rotation);//инстантим попап с циферкой
-           // healthPopUp.Hit(damage);
-            healthPopUp.Hit(Random.Range(0,10));
-        }
-        else
-        {
-            if (!isDead)
-            {
-                isDead = true;
-                StartCoroutine(Dead_Cor());
-            }
-        }
-    }
-    private IEnumerator Dead_Cor()
-    {
-
-        GameObject fx = Instantiate(deadFX_prefab, transform.position, transform.rotation);
-        yield return new WaitForSeconds(0.15f);
-        mr.enabled = false;
     }
 }
