2810d7433e4981f2ac4a247834b640202222086f
Fjodor Vinokurov <vinnokurov@googlemail.com>
2022-11-14 10:49:26 +0300

    подтюнил эффект смерти

---
+++ Assets/FX/Scripts/Visual/DeadFX.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AntFarm
+{
+    public class DeadFX : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
