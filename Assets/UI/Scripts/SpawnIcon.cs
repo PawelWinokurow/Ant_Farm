3b8473c20925ee0cf3795a959bacf6798e17b146
Fjodor Vinokurov <vinnokurov@googlemail.com>
2022-12-30 20:05:51 +0300

    подтюнил

--- Assets/UI/Scripts/SpawnIcon.cs
+++ Assets/UI/Scripts/SpawnIcon.cs
@@ -8,8 +8,10 @@ public class SpawnIcon : MonoBehaviour
 {
     public Transform item;
     public TMP_Text tmp;
+    public CanvasGroup canvasGroup;
     private Vector3 startPos;
     private Sequence sequence;
+
     void Start()
     {
         startPos = item.transform.localPosition;
@@ -17,9 +19,13 @@ public class SpawnIcon : MonoBehaviour
 
     public void Play(float delay, int count)
     {
+        sequence?.Kill();
         tmp.text = "x"+ count.ToString();
+        canvasGroup.alpha = 1f;
+        item.localPosition = startPos;
         sequence = DOTween.Sequence();
         sequence.Append(item.DOLocalMove(Vector3.zero, 0.5f).SetDelay(delay));
-        sequence.Append(item.DOLocalMove(startPos, 0.5f).SetDelay(1f- delay));
+        //sequence.Append(item.DOLocalMove(startPos, 0.5f).SetDelay(1f- delay));
+        sequence.Append(canvasGroup.DOFade(0,0.5f).SetDelay(1f - delay));
     }
 }
