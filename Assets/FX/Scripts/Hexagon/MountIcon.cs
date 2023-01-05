2c8ca939fbf886cf9a208893a7462ac355c90e34
Fjodor Vinokurov <vinnokurov@googlemail.com>
2023-01-05 10:07:03 +0300

    подтюнил

--- Assets/FX/Scripts/Hexagon/MountIcon.cs
+++ Assets/FX/Scripts/Hexagon/MountIcon.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
-
+using DG.Tweening;
 
 public class MountIcon : MonoBehaviour
 {
     public WorkHexagon mountIconPrefab;
     public WorkHexagon scaledIconPrefab;
+    private Vector3 scl;
+
+    private void Start()
+    {
+        scl = scaledIconPrefab.transform.localScale;
+        scaledIconPrefab.transform.localScale = Vector3.zero;
+        scaledIconPrefab.transform.DOScale(scl, 0.1f).SetEase(Ease.Linear);
+    }
 }
